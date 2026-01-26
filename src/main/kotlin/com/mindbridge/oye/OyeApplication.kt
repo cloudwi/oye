ebb50142a4148368ea841fb920d3b2e1565e5003
cloudwi <cloudwi@naver.com>
2026-01-26 21:13:49 +0900

    init

---
+++ src/main/kotlin/com/mindbridge/oye/OyeApplication.kt
@@ -0,0 +1,11 @@
+package com.mindbridge.oye
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OyeApplication
+
+fun main(args: Array<String>) {
+    runApplication<OyeApplication>(*args)
+}
---
+++ src/test/kotlin/com/mindbridge/oye/OyeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mindbridge.oye
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OyeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
